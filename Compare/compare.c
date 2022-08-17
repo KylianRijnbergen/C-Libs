098dc1464c7e23b833fdbfc27004bb0efe8cac58
KylianRijnbergen <k.j.rijnbergen@student.utwente.nl>
2022-08-17 14:20:08 +0200

    Added min and max functions for floats, ints, and long longs

--- Compare/compare.c
+++ Compare/compare.c
@@ -19,3 +19,63 @@ double mind(double a, double b)
     }
     return b;
 }
+
+/* Max function for floats */
+float maxf(float a, float b)
+{
+    if(a >= b)
+    {
+        return a;
+    }
+    return b;
+}
+
+/* Min function for floats */
+float minf(float a, float b)
+{
+    if (a <= b)
+    {
+        return a;
+    }
+    return b;
+}
+
+/* Max function for ints */
+int maxi(int a, int b)
+{
+    if(a >= b)
+    {
+        return a;
+    }
+    return b;
+}
+
+/* Min function for ints */
+int mini(int a, int b)
+{
+    if (a <= b)
+    {
+        return a;
+    }
+    return b;
+}
+
+/* Max function for long longs */
+long long maxll(long long a, long long b)
+{
+    if(a >= b)
+    {
+        return a;
+    }
+    return b;
+}
+
+/* Min function for long longs */
+long long minll(long long a, long long b)
+{
+    if (a <= b)
+    {
+        return a;
+    }
+    return b;
+}
--- Compare/compare.h
+++ Compare/compare.h
@@ -2,4 +2,10 @@
 #define COMPARE_H
 double maxd(double a, double b);
 double mind(double a, double b);
+float maxf(float a, float b);
+float minf(float a, float b);
+int maxi(int a, int b);
+int mini(int a, int b);
+long long maxll(long long a, long long b);
+long long minll(long long a, long long b);
 #endif
